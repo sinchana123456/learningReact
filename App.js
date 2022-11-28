170036f8b445ba9a081adb45a1cf4299411f9fa5
sinchana123456 <113234656+sinchana123456@users.noreply.github.com>
2022-11-28 19:30:41 +0530

    Expenses

---
+++ App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Lets get Started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
