4416388e1f7f1d0ee1e0d4e4428d76b8f6b4a272
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2022-11-29 19:42:54 +0530

    Expenses

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,23 +1,19 @@
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props){
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year=props.date.getFullYear();
-
     return (
-    <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
-        </div>
-        <div>{props.date.toISOString()}</div>
-        <div className="expense-item__description">
+        
+    <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
             <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
         </div>
-        </div>
+        </Card>
+        
         );
 }
 
