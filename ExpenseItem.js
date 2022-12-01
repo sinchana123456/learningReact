8b2acf131e37e0374173e52913ed94ad15dce057
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2022-12-01 15:07:48 +0530

    adding button by using useState

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
+
 const ExpenseItem = (props) => {
+    const [amount, setAmount] = useState(props.amount);
+
     const clickHandler = () => {
-        console.log('Clicked!!!!!!!!')
-    }
-    return (
-        
+        setAmount('100$');
+        console.log(amount);
+    };
+
+    return (  
     <Card className='expense-item'>
         <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
         <div className='expense-item__price'>Rs. {props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Delete</button>
+        <button onClick={clickHandler}>Change Expense</button>
         </Card>
         
         );
