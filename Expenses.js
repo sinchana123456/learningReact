d852d2f38df4d3098918d3559c05d2dc969bfc9e
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2022-12-04 19:04:56 +0530

    Conditional Rendering

--- Expenses.js
+++ Expenses.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+
 import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -14,17 +15,12 @@ const filteredExpenses = props.items.filter(expense => {
   return expense.date.getFullYear().toString() === filteredYear;
 });
 
+
+
 return (
     <Card className="expenses">
       <ExpenseFilter selected ={filteredYear} onChangeFilter={filterChangeHandler} />
-      {filteredExpenses.map((expense, index) => (
-      <ExpenseItem 
-      key={expense.id}
-      title={expense.title} 
-      amount={expense.amount} 
-      date={expense.date} 
-      />
-      ))};
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 };
