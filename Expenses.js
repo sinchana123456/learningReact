96ad9b5162e0bdf0b0914523f786c6dfe3c3f953
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2022-12-07 06:34:06 +0530

    Chart App styling chartBars

--- Expenses.js
+++ Expenses.js
@@ -3,7 +3,10 @@ import React, { useState } from 'react';
 import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+
 import './Expenses.css';
+
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -18,10 +21,13 @@ const filteredExpenses = props.items.filter(expense => {
 
 
 return (
+  <div>
     <Card className="expenses">
       <ExpenseFilter selected ={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses}/>
     </Card>
+    </div>
   );
 };
 
